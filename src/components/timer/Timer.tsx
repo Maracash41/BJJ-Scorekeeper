7deabf2d0b36aea22e69457385958dfbe217a40e
Maracash41 <9531945@gmail.com>
2024-11-03 16:56:18 +0300

    feat: add setTimer

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -6,6 +6,10 @@ const Timer: React.FC = () => {
   const [over, setOver] = useState(false);
   const [[m, s], setTime] = useState([5, 0]);
 
+  const [inputMinutes, setInputMinutes] = useState(0);
+  const [inputSeconds, setInputSeconds] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const timerContorl = (): void => {
     if (paused || over) return;
     if (m === 0 && s === 0) {
@@ -23,6 +27,20 @@ const Timer: React.FC = () => {
     setPaused(false);
   };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const setTimer = () => {
+    setTime([inputMinutes, inputSeconds]);
+    setPaused(false);
+    closeModal();
+  };
+
   useEffect(() => {
     const actveTimer = setInterval(() => timerContorl(), 1000);
     return () => clearInterval(actveTimer);
@@ -30,9 +48,39 @@ const Timer: React.FC = () => {
   return (
     <>
       <div className={classes.timer}>
-        <p>{`${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}`}</p>
-        <button onClick={() => setPaused(!paused)}>Pause</button>
+        <p
+          className={classes.timerTime}
+        >{`${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}`}</p>
+        <button
+          onClick={() => setPaused(!paused)}
+        >{`${paused ? "Start" : "Pause"}`}</button>
         <button onClick={() => reset()}>Reset</button>
+        <button onClick={openModal}>Set Timer</button>
+        {isModalOpen && (
+          <div className={classes.modal}>
+            <div className={classes.modalContent}>
+              <h2 className={classes.modalHeading}>Set Timer</h2>
+              <label>
+                Minutes:
+                <input
+                  type="number"
+                  value={inputMinutes}
+                  onChange={(e) => setInputMinutes(parseInt(e.target.value))}
+                />
+              </label>
+              <label>
+                Seconds:
+                <input
+                  type="number"
+                  value={inputSeconds}
+                  onChange={(e) => setInputSeconds(parseInt(e.target.value))}
+                />
+              </label>
+              <button onClick={setTimer}>Set Timer</button>
+              <button onClick={closeModal}>Close</button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );

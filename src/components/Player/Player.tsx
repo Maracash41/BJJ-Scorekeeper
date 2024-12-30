0960369cfa7865e901bbd3d0bf0f2d8f52397362
Maracash41 <9531945@gmail.com>
2024-12-30 17:37:41 +0300

    refactor: add the edit button for the player name

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -11,9 +11,7 @@ interface PlayerProps {
 
 const Player: React.FC<PlayerProps> = ({ player, changeName }) => {
   const [currentPlayerName, setCurrentPlayerName] = useState(player.name);
-  const [playerTeam, setPlayerTeam] = useState("No Team");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   const changePlayerNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -21,65 +19,25 @@ const Player: React.FC<PlayerProps> = ({ player, changeName }) => {
     setCurrentPlayerName(value);
   };
 
-  const changePlayerTeamHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setPlayerTeam(value);
-  };
-
-  const fileLogoHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file: File | null = e.target.files ? e.target.files[0] : null;
-
-    if (file) {
-      const blobUrl: string = URL.createObjectURL(file);
-      setImageSrc(blobUrl);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      if (imageSrc) {
-        URL.revokeObjectURL(imageSrc);
-      }
-    };
-  }, [imageSrc]);
-
   useEffect(() => {
     changeName(player.id, currentPlayerName);
   }, [currentPlayerName]);
 
   return (
     <div className={classes.player}>
-      <input
+      <div className={classes.playerContainer}>
+        <p className={classes.playerName}>{currentPlayerName}</p>
+        <button className={classes.playerEditButton}>&#9998;</button>
+      </div>
+      {/* <input
         type="text"
         className={classes.playerName}
         onChange={changePlayerNameHandler}
         value={currentPlayerName}
         maxLength={12}
-      />
-      {/* <label className={classes.team}>
-        Team:
-        <input
-          type="text"
-          className={classes.playerTeam}
-          onChange={changePlayerTeamHandler}
-          value={playerTeam}
-        />
-      </label>
-
-      <label className={classes.teamLogo}>
-        Logo:
-        <input
-          type="file"
-          accept="image/*"
-          className={classes.playerTeam}
-          onChange={fileLogoHandler}
-        />
-      </label>
-      {imageSrc && <img src={imageSrc} />} */}
+      /> */}
     </div>
   );
 };
 
 export default Player;
-
-//ToDo: add Player logo

247ec60b23396859ee9fe48de53a6d3b0b1a4ad0
Maracash41 <9531945@gmail.com>
2024-12-31 15:00:16 +0300

    feat: add open modal after click on the edit name button

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -19,23 +19,47 @@ const Player: React.FC<PlayerProps> = ({ player, changeName }) => {
     setCurrentPlayerName(value);
   };
 
+  const playerEditButtonHandler = () => {
+    switchModalOpenClose();
+  };
+
+  const switchModalOpenClose = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   useEffect(() => {
     changeName(player.id, currentPlayerName);
   }, [currentPlayerName]);
 
   return (
     <div className={classes.player}>
+      {isModalOpen && (
+        <div className={classes.playerEditModal}>
+          <div className={classes.playerEditModalContent}>
+            <div className={classes.playerEditModalInputs}>
+              <label>
+                Name:
+                <input
+                  type="text"
+                  className={classes.playerName}
+                  onChange={changePlayerNameHandler}
+                  placeholder={"Name"}
+                  maxLength={12}
+                />
+              </label>
+            </div>
+          </div>
+        </div>
+      )}
       <div className={classes.playerContainer}>
         <p className={classes.playerName}>{currentPlayerName}</p>
-        <button className={classes.playerEditButton}>&#9998;</button>
+        <button
+          className={classes.playerEditButton}
+          onClick={playerEditButtonHandler}
+        >
+          &#9998;
+        </button>
       </div>
-      {/* <input
-        type="text"
-        className={classes.playerName}
-        onChange={changePlayerNameHandler}
-        value={currentPlayerName}
-        maxLength={12}
-      /> */}
     </div>
   );
 };

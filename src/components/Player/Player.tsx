acb87bee37a991f691fa76b59bbf5260f4e8a078
Maracash41 <9531945@gmail.com>
2025-01-06 17:22:48 +0300

    feat: add modal window for editing player name

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classes from "./player.module.css";
 import { IPlayer } from "../intefaces/interfaces";
 
@@ -11,13 +11,8 @@ interface PlayerProps {
 
 const Player: React.FC<PlayerProps> = ({ player, changeName }) => {
   const [currentPlayerName, setCurrentPlayerName] = useState(player.name);
+  const [tempPlayerName, setTempPlayerName] = useState<string>("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const changePlayerNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value.length === 0) return;
-    setCurrentPlayerName(value);
-  };
 
   const playerEditButtonHandler = () => {
     switchModalOpenClose();
@@ -27,26 +22,68 @@ const Player: React.FC<PlayerProps> = ({ player, changeName }) => {
     setIsModalOpen(!isModalOpen);
   };
 
-  useEffect(() => {
+  const setPlayerName = () => {
+    if (tempPlayerName.length === 0 || tempPlayerName.length === 1) return;
+    setCurrentPlayerName(tempPlayerName);
     changeName(player.id, currentPlayerName);
-  }, [currentPlayerName]);
+  };
+
+  const changePlayerNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length === 0) return;
+    setTempPlayerName(value);
+  };
+
+  const closeButton = () => {
+    setPlayerName();
+    switchModalOpenClose();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key !== "Enter") return;
+    console.log("Enter was pressed", tempPlayerName);
+
+    closeButton();
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isModalOpen && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isModalOpen]);
 
   return (
     <div className={classes.player}>
       {isModalOpen && (
-        <div className={classes.playerEditModal}>
+        <div
+          className={classes.playerEditModal}
+          tabIndex={0}
+          onKeyDown={handleKeyDown}
+        >
           <div className={classes.playerEditModalContent}>
             <div className={classes.playerEditModalInputs}>
-              <label>
+              <label className={classes.playerEditModalLabel}>
                 Name:
                 <input
                   type="text"
-                  className={classes.playerName}
-                  onChange={changePlayerNameHandler}
-                  placeholder={"Name"}
+                  className={classes.playerEditModalInput}
+                  placeholder={"Type your name"}
                   maxLength={12}
+                  onChange={changePlayerNameHandler}
+                  ref={inputRef}
                 />
               </label>
+            </div>
+            <div className={classes.playerEditModalButtons}>
+              <button
+                className={classes.button + " " + classes.playerEditModalButton}
+                onClick={closeButton}
+                onKeyDown={handleKeyDown}
+              >
+                Close
+              </button>
             </div>
           </div>
         </div>

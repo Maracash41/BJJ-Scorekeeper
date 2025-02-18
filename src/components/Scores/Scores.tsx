f3b73a27c3ff8d4d5a6f0c3699430c5ff002f173
Maracash41 <9531945@gmail.com>
2025-02-18 14:06:01 +0300

    fix: reduce font size for footer

--- src/components/Scores/Scores.tsx
+++ src/components/Scores/Scores.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Player from "../Player/Player";
 import classes from "./scores.module.css";
 import { IPlayer } from "../intefaces/interfaces";
